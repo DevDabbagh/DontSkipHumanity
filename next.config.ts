b5aae4c253af56e51eccdf0faa4c0e68818f70ce
Ahmed Dabbagh <a.dabbagh@newline.ps>
2026-07-20 04:54:44 +0300

    feat: setup rewrites for admin dashboard

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,18 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-
+  async rewrites() {
+    return [
+      {
+        source: "/admin",
+        destination: "https://dsh-dashboard.vercel.app/admin",
+      },
+      {
+        source: "/admin/:path*",
+        destination: "https://dsh-dashboard.vercel.app/admin/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
